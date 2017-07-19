35cab0a3f07529eb99f82b62190269fdee14a360
shaowenhan <1138500211@qq.com>
2017-07-19 16:09:28 +0800

    finding a problem arising because of the edit in the two dimensional list and fix it by adding function copy_2_dimension_list and add a new test to check price function

--- cutting_stock.py
+++ cutting_stock.py
@@ -3,10 +3,29 @@ cutting stock problem: there is m orders and their widths are stored in list w
 ,their demand are stored in list n and the total width of the paper capacity
 this problem will return two lists with the cutting patterns and number of them 
 """
+from Simplex import *
+
+
+def copy_two_dimension_list(l):
+    ll = []
+    for i in l:
+        tmp = []
+        for j in i:
+            tmp.append(j)
+        ll.append(tmp)
+    return ll
 
 
 def cutting_stock(w, n, capacity):
-    initial_solution = get_initial_solution(w, capacity)
+    a = get_initial_solution(w, capacity)
+    row = column = len(w)
+    c = [1] * n
+    while True:
+        aa = copy_two_dimension_list(a)
+        nn = copy_two_dimension_list(n)
+        cc = copy_two_dimension_list(c)
+        result = simplex(-1, row, column, aa, nn, cc)
+        dual_result = solve_dual(result, row, column, aa, nn, cc)
 
 
 def get_initial_solution(w, capacity):
@@ -15,3 +34,13 @@ def get_initial_solution(w, capacity):
     for i in range(l):
         patterns[i][i] = capacity // w[i]
     return patterns
+
+
+def price(a, c, dual_solution):
+    new_c = []
+    for j in range(len(c)):
+        tmp = 0
+        for i in range(len(a)):
+            tmp += dual_solution[i] * a[i][j]
+        new_c.append(1 - tmp)
+    return new_c
