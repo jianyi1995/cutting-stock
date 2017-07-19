58a6f1032bceb79212228d4150085d2456a9ca91
shaowenhan <1138500211@qq.com>
2017-07-19 13:56:47 +0800

    it can return the number of each item it chooses

--- knapsack.py
+++ knapsack.py
@@ -1,16 +1,29 @@
 '''
 w means the weight of each item
 v means the value of each item
-capcity means the total weight of the knapsack
+capacity means the total weight of the knapsack
+
+value_table[i] means the maximum value we can get after using weight i of the knapsack
+decision_table means the number of each item we choose
 '''
 
 
 def knapsack(w, v, capacity):
     value_table = [0] * (capacity + 1)
-    choose_table = []
+    choose_table = [-1]
+    decision_table = [0] * len(w)
     for i in range(1, capacity + 1):
+        tmp = -1
         for j in range(len(w)):
-            if i - w[j] >= 0:
-                value_table[i] = max(value_table[i], value_table[i - w[j]] + v[j])
-    return value_table[capacity]
+            if i - w[j] >= 0 and value_table[i - w[j]] + v[j] > value_table[i]:
+                value_table[i] = value_table[i - w[j]] + v[j]
+                tmp = j
+        choose_table.append(tmp)
+    p = choose_table[-1]
+    c = capacity
+    while p != -1:
+        decision_table[p] += 1
+        c -= w[p]
+        p = choose_table[c]
+    return value_table[capacity], decision_table
 
--- test_knapsack.py
+++ test_knapsack.py
@@ -8,7 +8,20 @@ class TestSimplex(TestCase):
         w = [3, 8, 5]
         v = [4, 6, 5]
         capacity = 8
-        self.assertEqual(knapsack(w, v, capacity), 9)
+        self.assertEqual(knapsack(w, v, capacity)[0], 9)
+
+    def test_decision(self):
+        w = [3, 8, 5]
+        v = [4, 6, 5]
+        capacity = 8
+        self.assertEqual(knapsack(w, v, capacity)[1], [1, 0, 1])
+
+    def test_1(self):
+        v = [10, 40, 30, 50]
+        w = [5, 4, 6, 3]
+        capacity = 10
+        self.assertEqual(knapsack(w, v, capacity)[0], 150)
+        self.assertEqual(knapsack(w, v, capacity)[1], [0, 0, 0, 3])
 
 
 if __name__ == '__main__':
